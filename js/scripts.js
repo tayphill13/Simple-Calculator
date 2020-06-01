865fdf5b9374b60f9601c64c5b99033932f30da0
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-01 11:07:09 -0700

    add subtract and multiply functions
    Co-Authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -2,6 +2,14 @@ const add = function (number1, number2) {
   return number1 + number2;
 };
 
+const subtract = function (number1, number2) {
+  return number1 - number2;
+};
+
+const multiply = function (number1, number2) {
+  return number1 * number2;
+};
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
