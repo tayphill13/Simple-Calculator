747e071ea473e3d7198eaae08196756f7694d525
Taylor Phillips <taylorphillips133@gmail.com>
2020-06-01 11:08:27 -0700

    add divide function
    Co-Authored-by: Sara Kane <kane.sarak@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -10,6 +10,10 @@ const multiply = function (number1, number2) {
   return number1 * number2;
 };
 
+const divide = function (number1, number2) {
+  return number1 / number2;
+};
+
 const number1 = parseInt(prompt("Enter a number:"));
 const number2 = parseInt(prompt("Enter another number:"));
 
